bd87baef98168a1c66a3e6f6c923550a3e6050db
Gilgamesh27 <rashidul.islam34.ri@gmail.com>
2025-08-01 21:23:10 +0600

    changes includde: -added scene switching

--- src/main/java/com/oop/groupfourteen/group14simulating_operations_of_titas_gas/Alif/DashboardController.java
+++ src/main/java/com/oop/groupfourteen/group14simulating_operations_of_titas_gas/Alif/DashboardController.java
@@ -1,7 +1,33 @@
 package com.oop.groupfourteen.group14simulating_operations_of_titas_gas.Alif;
 
-public class DashboardController
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+
+public class DashboardController {
+
+    @FXML
+    private StackPane contentPane;
+
+    public void loadScene(String fxmlFilePath) {
+        try {
+            Node content = FXMLLoader.load(getClass().getResource(fxmlFilePath));
+            contentPane.getChildren().setAll(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void onOption1Click() {
+        loadScene("/com/oop/groupfourteen/group14simulating_operations_of_titas_gas/Alif/GasSupplyDashboard.fxml");
+    }
+
+    @FXML
+    public void onOption2Click() {
+        loadScene("/com/oop/groupfourteen/group14simulating_operations_of_titas_gas/Alif/BillingandMeteringOfficerDashboard.fxml");
+    }
+}
