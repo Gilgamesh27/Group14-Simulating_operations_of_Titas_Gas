448c2a11f0c83a5ca82b8541032a0d4ed12db7a1
Gilgamesh27 <rashidul.islam34.ri@gmail.com>
2025-08-13 19:29:52 +0600

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.oop.groupfourteen.group14simulating_operations_of_titas_gas {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.oop.groupfourteen.group14simulating_operations_of_titas_gas to javafx.fxml;
+    exports com.oop.groupfourteen.group14simulating_operations_of_titas_gas;
+}
