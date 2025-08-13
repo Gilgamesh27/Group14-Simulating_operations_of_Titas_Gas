4902885f2cbacfd3f14ae3cb4ef6aa43f28999ca
Gilgamesh27 <rashidul.islam34.ri@gmail.com>
2025-08-13 19:42:52 +0600

    Refactor LoginController and Main classes for improved structure and functionality

--- src/main/java/com/oop/groupfourteen/group14simulating_operations_of_titas_gas/LoginController.java
+++ src/main/java/com/oop/groupfourteen/group14simulating_operations_of_titas_gas/LoginController.java
@@ -1,7 +1,39 @@
 package com.oop.groupfourteen.group14simulating_operations_of_titas_gas;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController
 {
     @javafx.fxml.FXML
+    private Button forgotPasswordButton;
+    @javafx.fxml.FXML
+    private Button createAccountButton;
+    @javafx.fxml.FXML
+    private Label infoLabel;
+    @javafx.fxml.FXML
+    private TextField userIdField;
+    @javafx.fxml.FXML
+    private Button loginButton;
+    @javafx.fxml.FXML
+    private PasswordField passwordField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleForgotPassword(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleLogin(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleCreateAccount(ActionEvent actionEvent) {
+    }
+}
