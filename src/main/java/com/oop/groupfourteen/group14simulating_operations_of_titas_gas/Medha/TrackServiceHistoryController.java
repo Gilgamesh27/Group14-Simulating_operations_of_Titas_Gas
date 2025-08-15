c2b33950c427bba1956cae6f93b7a639886bc3f7
medha22224 <medha22224@gmail.com>
2025-08-16 00:39:58 +0600

    TrackServiceHistory

--- src/main/java/com/oop/groupfourteen/group14simulating_operations_of_titas_gas/Medha/TrackServiceHistoryController.java
+++ src/main/java/com/oop/groupfourteen/group14simulating_operations_of_titas_gas/Medha/TrackServiceHistoryController.java
@@ -19,17 +19,33 @@ public class TrackServiceHistoryController
 
     @javafx.fxml.FXML
     public void searchCustomerButton(ActionEvent actionEvent) {
+
+        if (customerIdField.getText().isEmpty()) {
+            statusLabel.setText("Please enter a customer ID.");
+        } else {
+            statusLabel.setText("Customer found: " + customerIdField.getText());
+        }
     }
 
     @javafx.fxml.FXML
     public void saveUpdatesButton(ActionEvent actionEvent) {
+
+        if (issueAnnotationField.getText().isEmpty()) {
+            statusLabel.setText("No updates to save.");
+        } else {
+            statusLabel.setText("Updates saved: " + issueAnnotationField.getText());
+        }
     }
 
     @javafx.fxml.FXML
     public void viewRecordsButton(ActionEvent actionEvent) {
+
+        statusLabel.setText("Displaying past service records for customer.");
     }
 
     @javafx.fxml.FXML
     public void validateRecordsButton(ActionEvent actionEvent) {
+
+        statusLabel.setText("Records validated successfully.");
     }
 }
